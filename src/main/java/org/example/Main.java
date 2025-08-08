11d22a34a9aab36968112e1dd9a289169733051e
Inara Oenning <inaraoenning@gmail.com>
2025-08-08 14:10:38 -0300

    public class Main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int bookPage = 500;
+        int bookYear = 2023;
+        String bookName = "Throne of Glass";
+        String bookAuthor = "Sarah J. Maas";
+
+        System.out.println(bookAuthor);
+        System.out.println(bookName);
+        System.out.println(bookPage);
+        System.out.println(bookYear);
+
+    }
+}
+
