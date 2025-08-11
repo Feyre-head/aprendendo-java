cdeb394e20d6939d104ebd4868e1b9c107e376d4
Inara Oenning <inaraoenning@gmail.com>
2025-08-11 11:51:37 -0300

    new Date();

---
+++ src/main/java/org/example/DataHora.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.util.Date;
+
+public class DataHora {
+
+    public static void main(String[] args){
+    Date clock = new Date();
+    System.out.println("Actual date:");
+    System.out.println(clock);
+    }
+}
