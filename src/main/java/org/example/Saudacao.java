531a857e563c5aa40e0a747a79ad3b23cffd6704
Feyre_head <inaraoenning@gmail.com>
2025-08-15 12:14:00 -0300

    ex Saudacao Hora

---
+++ src/main/java/org/example/Saudacao.java
@@ -0,0 +1,29 @@
+import java.time.LocalTime;
+
+public class Saudacao {
+
+    public static void main(String[] args) {
+
+//        Manhã → até 11h59
+//        Tarde → até 17h59
+//        Noite → depois disso
+
+        //pega hora atual do sistema
+        LocalTime horaAtual = LocalTime.now();
+
+        //converte para int e pega somente os numeros
+        int hora = horaAtual.getHour();
+
+        if (hora >= 0 & hora < 12) {
+                        System.out.println("Bom dia!");
+
+        } else if (hora >= 12 & hora <= 18) {
+                        System.out.println("Boa tarde!");
+
+        } else {
+            System.out.println("Boa noite!");
+        }
+
+
+    }
+}
