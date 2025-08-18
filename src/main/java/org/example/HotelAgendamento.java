c854f53fd2013c9d5dfee0938ae574b58d909421
Feyre_head <inaraoenning@gmail.com>
2025-08-17 21:27:50 -0300

    ex HotelAgendamento

---
+++ src/main/java/org/example/HotelAgendamento.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.Date;
+
+public class HotelAgendamento {
+
+    String guestName;
+    String roomNumber;
+    int numberOfNight;
+
+    Date data = new Date();
+    boolean isPaid = false;
+
+    public static void main(String[] args) {
+
+    }
+}
